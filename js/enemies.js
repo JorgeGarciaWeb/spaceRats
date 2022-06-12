d134cbae6c1b0f059c911ce1c7ffae97676b435f
Jorge Garcia <93034370+raspamarvel@users.noreply.github.com>
2022-06-12 17:05:05 +0200

    Update enemies.js

--- js/enemies.js
+++ js/enemies.js
@@ -1,27 +1,50 @@
 class Octopus {
-    constructor(ctx, enemyPosX, enemyPosY, enemyImg) {
+    constructor(ctx, octopusPosX, octopusPosY, octopusImg) {
 
         this.ctx = ctx
-        this.positionEnemy = { x: enemyPosX, y: enemyPosY }
+        this.positionEnemy = { x: octopusPosX, y: octopusPosY }
         this.enemySize = { w: 80, h: 80 }
-        this.enemyImg = enemyImg
+        this.octopusImg = octopusImg
         this.imageInstance = undefined
-
-        // this.init()
+    
+        this.init()
     }
 
-    // init() {
-    //     // this.ctx.fillStyle = 'red'
-    //     // this.ctx.fillRect(100, 100, 100, 100)
-    //     this.imageInstance = new Image()
-    //     this.imageInstance.scr = "./img/octopus.png"
-    // }
+    init() {
+       /*  this.ctx.fillStyle = 'red'
+        this.ctx.fillRect(100, 100, 100, 100) */
+        this.imageInstance = new Image()
+        this.imageInstance.src = `./img/octopus.png` 
+
+   }
 
     draw() {
-        // this.ctx.drawImage(this.imageInstance, 100, 100, 50, 50)
+        this.ctx.drawImage(this.imageInstance, 100, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 200, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 300, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 400, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 500, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 600, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 700, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 800, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 900, 100,100,100)
+        this.ctx.drawImage(this.imageInstance, 1000, 100,100,100)
 
-        //Enemies first row
-        this.ctx.fillStyle = 'red'
+
+
+        this.ctx.drawImage(this.imageInstance, 100, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 200, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 300, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 400, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 500, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 600, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 700, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 800, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 900, 200, 100, 100)
+        this.ctx.drawImage(this.imageInstance, 1000, 200, 100, 100) 
+ 
+         //Enemies first row
+        /* this.ctx.fillStyle = 'red'
         this.ctx.fillRect(100, 100, 50, 50)
         this.ctx.fillRect(200, 100, 50, 50)
         this.ctx.fillRect(300, 100, 50, 50)
@@ -34,9 +57,9 @@ class Octopus {
         this.ctx.fillRect(900, 100, 50, 50)
         this.ctx.fillRect(1000, 100, 50, 50)
         this.ctx.fillRect(1100, 100, 50, 50)
-
+ */
         //Enemies second row
-        this.ctx.fillStyle = 'red'
+       /*  this.ctx.fillStyle = 'red'
         this.ctx.fillRect(100, 200, 50, 50)
         this.ctx.fillRect(200, 200, 50, 50)
         this.ctx.fillRect(300, 200, 50, 50)
@@ -63,25 +86,21 @@ class Octopus {
         this.ctx.fillRect(800, 300, 50, 50)
         this.ctx.fillRect(900, 300, 50, 50)
         this.ctx.fillRect(1000, 300, 50, 50)
-        this.ctx.fillRect(1100, 300, 50, 50)
-
+        this.ctx.fillRect(1100, 300, 50, 50) */
+ 
     }
 }
 
 class Piton extends Octopus {
-    constructor(ctx, posX, posY) {
-        super(ctx, posX, posY)
+
+    constructor(ctx, pitonPosX, pitonPosY) {
+ 
+        super(ctx, pitonPosX, pitonPosY)
         this.ctx = ctx
-        this.enemytPos = { x: posX, y: posY }
-        this.enemySize = { w: 30, h: 30 }
-        this.imageInstance.src = 'img/piton.png'
+        this.pitonPos = { x: pitonPosX, y: pitonPosY }
+        this.pitonSize = { w: 30, h: 30 }
+        this.imageInstance = undefined
         // this.damage = 1.5
-    }
+    } 
+
 }
-
-
-
-
-
-
-
